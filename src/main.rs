642c8b81b1818fd9056919cb7ea166453703157d
Lukas Alberto Belck <lukas@seedtrace.org>
2024-05-17 02:25:12 +0200

    fix: log only levels based on RUST_LOG env

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ use sqlx::postgres::PgPoolOptions;
 async fn main() -> anyhow::Result<()> {
     // load environment variables from .env file
     dotenv().ok();
+    let cfg = envy::from_env::<Config>().unwrap();
+
+    let rust_log = std::env::var("RUST_LOG").unwrap_or_else(|_| "info".to_string());
+    let rust_log_filter =
+        tracing::level_filters::LevelFilter::from(rust_log.parse::<tracing::Level>().unwrap());
 
     let subscriber = tracing_subscriber::fmt()
         .compact()
+        .with_max_level(rust_log_filter)
         .with_file(true)
         .with_line_number(true)
         .with_thread_ids(true)
@@ -23,13 +29,11 @@ async fn main() -> anyhow::Result<()> {
     tracing::subscriber::set_global_default(subscriber).unwrap();
     tracing::info!("Starting application up");
 
-    let cfg = envy::from_env::<Config>().unwrap();
-
     let pool = PgPoolOptions::new()
         .max_connections(10)
         .connect(&cfg.database_url)
         .await?;
-    tracing::info!("Connected to database");
+    tracing::debug!("Connected to database");
 
     api::serve(cfg, pool).await?;
     Ok(())
